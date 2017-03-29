20ed9f5baa0f9f5607ff4a060a401528ec33c858
cal <callum1234moo@gmail.com>
2017-03-28 20:05:25 -0400

    automatically setup tpm and vundle on init

--- init.sh
+++ init.sh
@@ -2,5 +2,13 @@
 
 set -e
 
+# link the confs
 ln tmux.conf ~/.tmux.conf
 ln vimrc ~/.vimrc
+
+# install tpm
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# intall vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
