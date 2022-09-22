a9936e68336a054fb072b9d79f631ecd3054795c
Callum Smith <cal@stripe.com>
2022-09-21 20:54:48 -0400

    misc updates

--- config.fish
+++ config.fish
@@ -9,6 +9,9 @@ alias gpl="git pull"
 alias gs="git status"
 alias gb="git branch"
 alias gc="git checkout"
+alias gplo="git pull origin"
+alias gplom="git pull origin master"
+alias gfom="git fetch origin master"
 
 # ubuntu defaults python to python3
 if test (uname) = "Linux"
@@ -28,3 +31,15 @@ set -x NVM_DIR ~/cal/.nvm
 
 set -x PATH $PATH $HOME/.cargo/bin
 fnm env | source
+
+# stripe conf
+source (rbenv init -|psub)
+# source (nodenv init -|psub)
+source ~/stripe/space-commander/bin/sc-env-activate.fish
+
+set PATH $HOME/stripe/space-commander/bin $PATH
+set PATH $HOME/stripe/password-vault/bin $PATH
+set PATH $HOME/stripe/henson/bin $PATH
+set PATH $HOME/.rbenvs/shims $PATH
+set PATH $HOME/.rbenvs/bin $PATH
+alias stripe-preview=/Users/$USER/stripe/stripe-preview/stripe-preview
